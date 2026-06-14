7a324e91d9fc6fa45e7af98eaf8a1978a651bb26
Виктор <20viktor120@gmail.com>
2026-06-14 18:03:25 +0300

    homework_30_1

---
+++ materials/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+
+
+class Course(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name="Название курса",
+        help_text="Укажите название курса",
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="Описание курса",
+        help_text="Укажите описание курса",
+    )
+    preview = models.ImageField(
+        upload_to="course_previews/",
+        blank=True,
+        null=True,
+        verbose_name="Превью (картинка)",
+    )
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+    def __str__(self):
+        return self.name
+
+
+class Lesson(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name="Название урока",
+        help_text="Укажите название урока",
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="Описание урока",
+        help_text="Укажите описание урока",
+    )
+    preview = models.ImageField(
+        upload_to="lesson_previews/",
+        blank=True,
+        null=True,
+        verbose_name="Превью (картинка)",
+    )
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name="lessons",
+        verbose_name="Курс",
+        blank=True,
+        null=True,
+    )
+    video_link = models.URLField(verbose_name="Ссылка на видео", blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
+
+    def __str__(self):
+        return f"{self.name} (курс: {self.course.name})"
---
+++ users/models.py
@@ -0,0 +1,42 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(
+        unique=True, verbose_name="Почта", help_text="Укажите почту"
+    )
+
+    phone = models.CharField(
+        max_length=35,
+        blank=True,
+        null=True,
+        verbose_name="Телефон",
+        help_text="Введите номер телефона",
+    )
+    city = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True,
+        verbose_name="Город",
+        help_text="Введите город",
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatars",
+        blank=True,
+        null=True,
+        verbose_name="Аватарка",
+        help_text="Добавьте фото",
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email
